d176b63e25ad99748c128c7f46994c1123701895
Ieuan Kessels <ieuan@ruun.nl>
2023-02-20 23:09:17 +0100

    Latest changes

--- src/components/structural/Header.tsx
+++ src/components/structural/Header.tsx
@@ -1,30 +1,30 @@
+import {faDiscord}        from "@fortawesome/free-brands-svg-icons";
 import {faBars, faPerson} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon}  from "@fortawesome/react-fontawesome";
-import React              from "react";
 import Logo               from "assets/logo.png";
 import Aside              from "components/structural/Aside";
-import Button             from "components/visual/Button";
 import {Column, Row}      from "components/structural/Grid";
 import Group              from "components/structural/Group";
+import Button             from "components/visual/Button";
+import React              from "react";
 import "./Header.scss";
 
 class Header extends React.Component {
     render() {
         return (
             <>
-            <header className="header">
-                <Row>
-                    <Column className="justify-content-center"><Button color="secondary" toggle="#aside-sidebar-left"><FontAwesomeIcon icon={faBars} /></Button></Column>
-                    <Column className="justify-content-center align-items-center"><a className={"nav-brand logo"} href={"/"}><img src={Logo} alt="logo" /></a></Column>
-                    <Column className="justify-content-center align-items-end">
-                        <Group spacing="1em" className="hidden-mobile">
-                            <Button href="https://discord.gg/artbot" color="secondary" className="btn-text-light">Join Discord</Button>
-                            <Button color="secondary" toggle='#aside-login' className="btn-text-light">Log In</Button>
-                        </Group>
-                        <Button color="secondary" className="btn-text-light hidden-desktop"><FontAwesomeIcon icon={faPerson}/></Button>
-                    </Column>
-                </Row>
-            </header>
+                <header className="header">
+                    <Row>
+                        <Column className="justify-content-center"><Button color="secondary" className="btn-icon-only" toggle="#aside-sidebar-left"><FontAwesomeIcon icon={faBars} /></Button></Column>
+                        <Column className="justify-content-center align-items-center"><a className={"nav-brand logo"} href={"/"}><img src={Logo} alt="logo" /></a></Column>
+                        <Column className="justify-content-center align-items-end">
+                            <Group spacing="1em">
+                                <Button href="https://discord.gg/artbot" color="secondary" className="btn-text-light"><FontAwesomeIcon icon={faDiscord} /><span className="hidden-mobile"> Discord</span></Button>
+                                <Button color="secondary" toggle="#aside-login" className="btn-text-light"><FontAwesomeIcon icon={faPerson} /><span className="hidden-mobile"> Log In</span></Button>
+                            </Group>
+                        </Column>
+                    </Row>
+                </header>
                 <Aside id="aside-login" side="right">
                     <h1>Log in/Sign up</h1>
                 </Aside>
