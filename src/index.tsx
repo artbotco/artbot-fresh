1533b935750487e4d8e28ea7e4fe10bb660602f2
Ieuan Kessels <ieuan@sparksetters.com>
2023-03-07 17:58:05 +0100

    Show site on letsmakeamovie as well

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.scss';
-import Home from './pages/Home';
-import store from '_redux';
-import reportWebVitals from './reportWebVitals';
+import store                          from "_redux";
+import React                          from "react";
+import ReactDOM                       from "react-dom/client";
+import {Provider}                     from "react-redux";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {PersistGate}                  from "redux-persist/integration/react";
+import "./index.scss";
+import Home                           from "./pages/Home";
+import reportWebVitals                from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={store.__PERSISTOR} loading={null}>
-        <BrowserRouter>
-          <Routes>
-            <Route path={'/'} element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate persistor={store.__PERSISTOR} loading={null}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path={"/"} element={<Home />} />
+                        <Route path={"/letsmakeamovie"} element={<Home />} />
+                    </Routes>
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
